66b3dcde2dee02738687dd5845605dae8c8b4b75
gashetonwan <fran.carlom@outlook.com>
2026-07-13 02:33:01 -0500

    admincrud arregalr por webon

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,29 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import HomePublic from './pages/HomePublic';
-import AdminCrud from './pages/AdminCrud1.jsx';
-import DetalleDestino from './pages/DetalleDestino';
-import ProtectedRoute from './components/ProtectedRoute';
-import Login from './pages/login.jsx'
-import Register from './pages/Register.jsx';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import HomePublic from "./pages/HomePublic";
+import AdminCrud from "./pages/AdminCrud.jsx";
+import DetalleDestino from "./pages/DetalleDestino";
+import ProtectedRoute from "./components/ProtectedRoute";
+import Login from "./pages/login.jsx";
+import Register from "./pages/Register.jsx";
 
 function App() {
   return (
-    <BrowserRouter basename='/turismo_web'>
+    <BrowserRouter basename="/turismo_web">
       <Navbar />
       <Routes>
         <Route path="/" element={<HomePublic />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/destino/:id" element={<DetalleDestino />} />
-        <Route path="/admin" element={
-          <ProtectedRoute requiredRole="ADMIN">
-            <AdminCrud />
-          </ProtectedRoute>
-        } />
+        <Route
+          path="/admin"
+          element={
+            <ProtectedRoute requiredRole="ADMIN">
+              <AdminCrud />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
